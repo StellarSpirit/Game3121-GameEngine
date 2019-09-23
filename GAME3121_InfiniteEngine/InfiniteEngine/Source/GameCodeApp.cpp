cb4e388ad1e38bc322a8b1f66fd2fc7dfd9102fe
Jordan James <37217592+Benefit999@users.noreply.github.com>
2019-09-22 23:08:31 -0400

    Cpu Speed Check Implementation

--- GAME3121_InfiniteEngine/InfiniteEngine/Source/GameCodeApp.cpp
+++ GAME3121_InfiniteEngine/InfiniteEngine/Source/GameCodeApp.cpp
@@ -1,40 +1,48 @@
+#include <iostream>
 #include "GameCodeApp.h"
+
+using namespace std;
 
 GameCodeApp::GameCodeApp()
 {
-	mName = LPCSTR("Infinite Engine");
+
 }
 
-bool GameCodeApp::IsOnlyInstance(LPCTSTR gameTitle) {
-	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
-	if (GetLastError() != ERROR_SUCCESS) {
-		HWND hWnd = FindWindow(gameTitle, NULL);
-		if (hWnd) {
-			// An instance of your game is already running.
-			ShowWindow(hWnd, SW_SHOWNORMAL);
-			SetFocus(hWnd);
-			SetForegroundWindow(hWnd);
-			SetActiveWindow(hWnd);
-			return false;
-		}
-	}
-	return true;
-}
-
-bool GameCodeApp::CheckStorage(const DWORDLONG diskSpaceNeeded) {
-	// Check for enough free disk space on the current disk.
-	int const drive = _getdrive();
-	struct _diskfree_t diskfree;
-	_getdiskfree(drive, &diskfree);
-	unsigned __int64 const neededClusters = diskSpaceNeeded /
-		(diskfree.sectors_per_cluster *
-			diskfree.bytes_per_sector);
-
-	if (diskfree.avail_clusters < neededClusters) {
-		// if you get here you don’t have enough disk space!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		//GCC_ERROR("CheckStorage Failure: Not enough physical storage.");
-//#pragma GCC error "CheckStorage Failure: Not enough physical storage."
+bool GameCodeApp::CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNeeded) {
+	MEMORYSTATUSEX status;
+	GlobalMemoryStatusEx(&status);
+	if (status.ullTotalPhys < physicalRAMNeeded) {
+		/* You don't have enough physical memory.*/
+		cout << "CheckMemory Failure: Not enough physical memory.";
 		return false;
 	}
-	return true;
+	//Check for enough free memory.
+	if (status.ullAvailVirtual < virtualRAMNeeded) {
+		// You don't have enough virtual memory available.
+		cout << "CheckMemory Failure: Not enough virtual memory.";
+		return false;
+	}
+	char *buff = new char[virtualRAMNeeded];
+	if (buff)
+		delete[] buff;
+	else {
+		cout << "CheckMemory Failure: Not enough contiguous memory.";
+		return false;
+	}
+}
+
+DWORD GameCodeApp::ReadCPUSpeed() {
+	DWORD BufSize = sizeof(DWORD);
+	DWORD dwMHz = 0;
+	DWORD type = REG_DWORD;
+	HKEY hKey;
+	// open the key where the proc speed is hidden:
+	long lError = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0",
+		0, KEY_READ, &hKey);
+	if (lError == ERROR_SUCCESS) {
+		// query the key:
+		RegQueryValueEx(hKey, "MHz", NULL, &type, (LPBYTE)&dwMHz,
+			&BufSize);
+	}
+	return dwMHz;
 }
--- GAME3121_InfiniteEngine/InfiniteEngine/Source/GameCodeApp.h
+++ GAME3121_InfiniteEngine/InfiniteEngine/Source/GameCodeApp.h
@@ -1,15 +1,13 @@
 #pragma once
+
 #include "Windows.h"
-#include <stdio.h>
-#include <direct.h>
 
 class GameCodeApp
 {
 public:
-	LPCSTR mName;
 
 	GameCodeApp();
 
-	bool IsOnlyInstance(LPCTSTR gameTitle);
-	bool CheckStorage(const DWORDLONG diskSpaceNeeded);
+	bool CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNeeded);
+	DWORD ReadCPUSpeed();
 };

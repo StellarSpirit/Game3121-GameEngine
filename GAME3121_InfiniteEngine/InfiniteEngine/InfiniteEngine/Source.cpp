d1bf9086401d31429c1739184728350ce6c86b5d
Benefit999 <37217592+Benefit999@users.noreply.github.com>
2019-09-26 00:18:09 -0400

    Fixed Display Architecture

--- GAME3121_InfiniteEngine/InfiniteEngine/InfiniteEngine/Source.cpp
+++ GAME3121_InfiniteEngine/InfiniteEngine/InfiniteEngine/Source.cpp
@@ -7,25 +7,8 @@ int main()
 {
 	GameCodeApp* gGameCodeApp;
 	gGameCodeApp = new GameCodeApp();
-
-	if (gGameCodeApp->IsOnlyInstance(gGameCodeApp->mName))
-	{
-		cout << "This is the only instance of window " << gGameCodeApp->mName << endl;
-	}
-	else
-	{
-		cout << "There is another instance of window " << gGameCodeApp->mName << endl;
-	}
-
-	if (gGameCodeApp->CheckStorage(300000000))
-	{
-		cout << "The computer has sufficient storage space." << endl;
-	}
-	else
-	{
-		cout << "The computer does not have sufficient storage space." << endl;
-	}
-
+	gGameCodeApp->ReadCPUSpeed();
+	//cout << "\n";
 
 	system("PAUSE");
 	return 0;
--- GAME3121_InfiniteEngine/InfiniteEngine/Source/GameCodeApp.cpp
+++ GAME3121_InfiniteEngine/InfiniteEngine/Source/GameCodeApp.cpp
@@ -8,41 +8,26 @@ GameCodeApp::GameCodeApp()
 
 }
 
-bool GameCodeApp::CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNeeded) {
-	MEMORYSTATUSEX status;
-	GlobalMemoryStatusEx(&status);
-	if (status.ullTotalPhys < physicalRAMNeeded) {
-		/* You don't have enough physical memory.*/
-		cout << "CheckMemory Failure: Not enough physical memory.";
-		return false;
-	}
-	//Check for enough free memory.
-	if (status.ullAvailVirtual < virtualRAMNeeded) {
-		// You don't have enough virtual memory available.
-		cout << "CheckMemory Failure: Not enough virtual memory.";
-		return false;
-	}
-	char *buff = new char[virtualRAMNeeded];
-	if (buff)
-		delete[] buff;
-	else {
-		cout << "CheckMemory Failure: Not enough contiguous memory.";
-		return false;
-	}
-}
-
 DWORD GameCodeApp::ReadCPUSpeed() {
 	DWORD BufSize = sizeof(DWORD);
 	DWORD dwMHz = 0;
 	DWORD type = REG_DWORD;
+	DWORD type2 = REG_SZ;
+	char processor[1023];
 	HKEY hKey;
+	DWORD charSize = sizeof(processor);
 	// open the key where the proc speed is hidden:
 	long lError = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0",
 		0, KEY_READ, &hKey);
 	if (lError == ERROR_SUCCESS) {
 		// query the key:
-		RegQueryValueEx(hKey, "MHz", NULL, &type, (LPBYTE)&dwMHz,
+		RegQueryValueEx(hKey, "~MHz", NULL, &type, (LPBYTE)&dwMHz,
 			&BufSize);
+		RegQueryValueEx(hKey, "ProcessorNameString", NULL, &type2, (LPBYTE)&processor,
+			&charSize);
 	}
+
+	cout << "CPU Speed: " << dwMHz << " MHz\n";
+	cout << "Processor: " << processor << "\n";
 	return dwMHz;
 }

2a68eae8a68c4e9af600c08b431edb6477244c90
StellarSpirit <34275038+StellarSpirit@users.noreply.github.com>
2019-10-08 11:14:34 -0400

    Rearranged some of the files in the folder structure
    Moved files that were in project directory to Source folder

---
+++ GAME3121_InfiniteEngine/InfiniteEngine/Source/Source.cpp
@@ -0,0 +1,18 @@
+#include "GameCodeApp.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	GameCodeApp* gGameCodeApp;
+	gGameCodeApp = new GameCodeApp();
+	
+	//gGameCodeApp->CheckMemory();
+
+
+
+
+	system("PAUSE");
+	return 0;
+}

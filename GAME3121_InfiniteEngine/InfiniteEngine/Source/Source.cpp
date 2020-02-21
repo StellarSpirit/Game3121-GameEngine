1734b9c935a992c829563874c6ab12dd77552afa
Benefit999 <37217592+Benefit999@users.noreply.github.com>
2020-02-21 01:33:48 -0500

    Successful Test of SFML
    Able to get SFML functioning

--- GAME3121_InfiniteEngine/InfiniteEngine/Source/Source.cpp
+++ GAME3121_InfiniteEngine/InfiniteEngine/Source/Source.cpp
@@ -1,18 +1,24 @@
-#include "GameCodeApp.h"
-#include <iostream>
-
-using namespace std;
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	GameCodeApp* gGameCodeApp;
-	gGameCodeApp = new GameCodeApp();
-	
-	//gGameCodeApp->CheckMemory();
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 
-
-	system("PAUSE");
 	return 0;
 }

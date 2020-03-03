c0c1f817a3d0094d7cc1433c6f9328ccd9645ea4
StellarSpirit <34275038+StellarSpirit@users.noreply.github.com>
2020-03-02 23:15:27 -0500

    Splash Screen
    Added logo
    Implemented rough splash screen

--- GAME3121_InfiniteEngine/InfiniteEngine/Source/Source.cpp
+++ GAME3121_InfiniteEngine/InfiniteEngine/Source/Source.cpp
@@ -1,24 +1,43 @@
 #include <SFML/Graphics.hpp>
+#include "GameCodeApp.h"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	GameCodeApp gGameCodeApp = GameCodeApp();
+	sf::Clock clock;
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "INFINITE ENGINE");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+
+	sf::Time splashScreenDelay = sf::seconds(5.0f);
+
+	sf::Texture splashScreenImage;
+	splashScreenImage.loadFromFile("../Assets/Images/IELogoBlotches.png");
+	sf::Sprite splashScreenSprite;
+	splashScreenSprite.setTexture(splashScreenImage);
 
 	while (window.isOpen())
 	{
 		sf::Event event;
+		
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
+		
+		
 		window.clear();
 		window.draw(shape);
+		if (clock.getElapsedTime() < splashScreenDelay)
+			window.draw(splashScreenSprite);
 		window.display();
 	}
 
+	
+
+	
 	return 0;
 }
